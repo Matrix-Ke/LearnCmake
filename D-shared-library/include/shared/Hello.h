d4889f2866aac44debf3a3a2877de3a65332e7f1
Matrix <2513084541@qq.com>
2022-04-22 23:36:03 +0800

    practise

--- D-shared-library/include/shared/Hello.h
+++ D-shared-library/include/shared/Hello.h
@@ -1,7 +1,13 @@
 #ifndef __HELLO_H__
 #define __HELLO_H__
 
-class Hello
+#ifdef helloShared_EXPORTS
+#define HELLO_API  __declspec(dllexport)
+#else   
+#define HELLO_API  __declspec(dllimport)
+#endif // !helloShared_EXPORTS
+
+class HELLO_API Hello
 {
 public:
     void print();
