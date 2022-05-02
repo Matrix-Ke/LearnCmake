e8685653af09649cf433706381e44cbc69ab450b
MatrixK <2513084541@qq.com>
2022-05-02 11:52:21 +0800

    使用动态库

---
+++ CMakePractise/Demo01/include/MathFunctions.h
@@ -0,0 +1,11 @@
+
+#if defined(EXPORTING_MYMATH)
+#    define DECLSPEC __declspec(dllexport)
+#else
+#    define DECLSPEC __declspec(dllimport)
+#endif
+
+namespace mathfunctions
+{
+	double DECLSPEC sqrt(double x);
+}
