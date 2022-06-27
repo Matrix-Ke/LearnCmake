0ceead22aa6cc02d89c3a8a4441b1c844b58d4b0
Matrix <2513084541@qq.com>
2022-06-27 17:10:33 +0800

    test spdlog

--- CMakePractise/02_FetchContent/tutorial.cpp
+++ CMakePractise/02_FetchContent/tutorial.cpp
@@ -4,24 +4,29 @@
 #include <iostream>
 #include <string>
 #include "MathFunctions.h"
-
+#include "spdlog/spdlog.h"
 
 
 int main(int argc, char* argv[])
 {
-	if (argc < 2) {
-		std::cout << "Usage: " << argv[0] << " number" << std::endl;
-		return 1;
-	}
 
-	// convert input to double
-	const double inputValue = atof(argv[1]);
+	spdlog::info("Welcome to spdlog!");
 
 
-	const double outputValue = mathfunctions::sqrt(inputValue);
+	//if (argc < 2)
+	//{
+	//	std::cout << "Usage: " << argv[0] << " number" << std::endl;
+	//	return 1;
+	//}
+
+	//// convert input to double
+	//const double inputValue = atof(argv[1]);
 
 
-	std::cout << "The square root of " << inputValue << " is " << outputValue
-		<< std::endl;
+	//const double outputValue = mathfunctions::sqrt(inputValue);
+
+
+	//std::cout << "The square root of " << inputValue << " is " << outputValue
+	//	<< std::endl;
 	return 0;
 }
