a9fa9f8341f0158e0284a37060290f947eb63e8a
Matrix <2513084541@qq.com>
2022-06-27 19:27:30 +0800

    update

--- CMakePractise/02_FetchContent/CMake/spdlog.cmake
+++ CMakePractise/02_FetchContent/CMake/spdlog.cmake
@@ -24,7 +24,7 @@ Fetchcontent_declare(
     spdlog	#库名字
     GIT_REPOSITORY  git@github.com:gabime/spdlog.git	# 仓库地址
     GIT_TAG v1.x # 库版本
-    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/_deps/spdlog # 指定库下载地址
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/spdlog # 指定库下载地址
     )
 FetchContent_MakeAvailable(spdlog)
 
